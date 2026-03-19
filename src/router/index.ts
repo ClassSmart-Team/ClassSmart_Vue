fe6aad0ebb7f21abb4d474fb7100dc610702b615
jimena <jimenalerma96@gmail.com>
2026-03-19 14:01:28 -0600

    ruta de login básico

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,6 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import AdminRegistroView from '../views/AdminRegistroView.vue'
 import HomeGeneralView from '@/views/HomeGeneralView.vue'
+import LoginView from '@/views/LoginView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -16,6 +17,14 @@ const router = createRouter({
       component: AdminRegistroView,
       meta: { role: 'Administrador' }
     },
+
+   {
+      path: '/Login',
+      name: 'Login',
+      component: LoginView,
+    
+    },
+
   ],
 })
 
