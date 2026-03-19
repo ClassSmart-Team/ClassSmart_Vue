ab1510f10c937e922cfa9c353422de449dd12a14
jimena <jimenalerma96@gmail.com>
2026-03-19 10:29:34 -0600

    Creación de vista Home general

---
+++ src/views/HomeGeneralView.vue
@@ -0,0 +1,41 @@
+<script setup lang="ts">
+</script>
+
+<template>
+<Contenedor> 
+
+    <div class="contenido">
+ <div class="ContSmall center">
+    <h1>Pagina Home</h1>
+ </div>  
+    </div>
+ 
+
+ <div class="contenido" style="padding-top: 80px">
+  <div class="ContBig center">
+    <h2> Contenido de la pagina home </h2>
+</div>
+ </div>   
+  
+
+</Contenedor>
+</template>
+
+<style scoped>
+
+.ContSmall{
+  background: var(--color-Azul);
+  width: 1000px;
+  height:60px;
+  border-radius: 20px;
+}
+
+.ContBig{
+  background: var(--color-Blanco);
+  width: 800px;
+  height:400px;
+  border-radius: 20px;
+}
+
+
+</style>
