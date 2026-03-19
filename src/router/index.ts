9ba9394c7b390efb6f2ce0313e33195055c1a58f
jimena <jimenalerma96@gmail.com>
2026-03-19 12:26:25 -0600

    Intento de Navegation Guard y cambios en la pantalla general

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,6 +1,6 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
 import AdminRegistroView from '../views/AdminRegistroView.vue'
+import HomeGeneralView from '@/views/HomeGeneralView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -8,7 +8,7 @@ const router = createRouter({
     {
       path: '/',
       name: 'home',
-      component: HomeView,
+      component: HomeGeneralView,
     },
     {
       path: '/AdminRegister',
