a0028e4194c599a1f4ddf81f72fa6761a8147097
PalomaPortillo <24170057@uttcampus.edu.mx>
2026-03-25 16:53:46 -0600

    librerias

---
+++ src/types/types.ts
@@ -0,0 +1,37 @@
+export interface Register {
+  name: string
+  lastname: string
+  email: string
+  password: string
+  cellphone: string
+}
+
+export interface Login {
+  email: string
+  password: string
+}
+export interface SessionData {
+  token: string
+  user: {
+    id: number
+    name: string
+    lastname: string
+    email: string
+    cellphone: string
+    active: boolean
+    role: {
+      id: number
+      description: string
+      created_at: string
+      updated_at: string
+    }
+    created_at: string
+    updated_at: string
+  }
+}
+
+export interface GroupData {
+  period_id: number
+  name: string
+  description: string
+}
